8c2727cc20a1439d0e88299de65dc547308a824e
Tomona Sako <sakotomona@gmail.com>
2020-10-03 22:11:57 +0900

    added RecyclerView

--- app/src/main/java/com/example/assignment2_contacts/MainActivity.kt
+++ app/src/main/java/com/example/assignment2_contacts/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.assignment2_contacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.assignment2_contacts.contactList.ContactListAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = ContactListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
--- app/src/main/java/com/example/assignment2_contacts/contactList/ContactListAdapter.kt
+++ app/src/main/java/com/example/assignment2_contacts/contactList/ContactListAdapter.kt
@@ -1,4 +1,47 @@
 package com.example.assignment2_contacts.contactList
 
-class ContactListAdapter {
+import android.content.Context
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.assignment2_contacts.R
+import com.example.assignment2_contacts.database.Contact
+import kotlinx.android.synthetic.main.contact_item_view.view.*
+
+class ContactListAdapter internal constructor(
+    context: Context
+) : RecyclerView.Adapter<ContactListAdapter.ContactViewHolder>() {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+    private var contacts = emptyList<Contact>()
+
+    inner class ContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val contactNameItemView: TextView = itemView.findViewById(R.id.name_text_view)
+        val contactPhoneNumItemView: TextView = itemView.findViewById(R.id.phone_num_text_view)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+        val itemView = inflater.inflate(R.layout.contact_item_view, parent, false)
+        return ContactViewHolder(itemView)
+    }
+
+
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        val current = contacts[position]
+        holder.contactNameItemView.text = current.name
+        holder.contactPhoneNumItemView.text = current.phoneNumber
+    }
+
+    internal fun setContacts(contacts: List<Contact>) {
+        this.contacts = contacts
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int = contacts.size
+
+
+
 }
